b89b47eb7f3c8ba77c45a5e63fc07bc830b11619
historianking0004-sys <historianking0004@gmail.com>
2026-06-22 16:51:36 +0500

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-// Use Atlas URI from Render, fallback to local for dev
 const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/auth';
 
 mongoose.connect(uri)
